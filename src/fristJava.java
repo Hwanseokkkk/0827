ee0525a639d7b25138ed94c6e09e11bf1008a975
Hwanseokkkk <5038256@gmail.com>
2022-08-27 10:04:38 +0900

    ㅇㄴㄹ

--- src/fristJava.java
+++ src/fristJava.java
@@ -5,7 +5,7 @@ public class fristJava {
 		long value1 = Double.doubleToRawLongBits(avg);
 		System.out.println(Long.toBinaryString(value1));
 		//100000001010110011000000000000000000000000000000000000000000000 이렇게 저장되어있는데 자바는 읽을때 89.5로 불러온다
-		//룰이 있다...룰대로 읽는다.
+		//룰이 있다...룰대로 읽는다. 
 	}
 
 }
